7d0c7de0672782b00816e0dac3da55a0f97bfca7
SpecOps2016 <76586680+SpecOps2016@users.noreply.github.com>
2023-08-05 10:47:42 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['firebase/app'],
+      external: ['firebase/app', 'firebase/firestore'],
     },
   },
   base: './',
