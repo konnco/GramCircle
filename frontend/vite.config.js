2ed0e7ca401af0413b28661cdd33e2b5b2e3d311
SpecOps2016 <76586680+SpecOps2016@users.noreply.github.com>
2023-08-05 10:50:26 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/firestore'],
-    },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
   },
   base: './',
-});
+})
